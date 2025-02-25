0786ae5db98195df5f76a0e32732db1ff8a82ff9
Anthony <kojiro5656i@gmail.com>
2025-02-25 15:14:02 +0100

    p2c3-exercice

--- src/components/ShoppingList.js
+++ src/components/ShoppingList.js
@@ -1,19 +1,7 @@
-const plantList = [
-    'monstera',
-    'ficus lyrata',
-    'pothos argenté',
-    'yucca',
-    'palmier'
-]
+import { plantList } from '../datas/plantList'
 
 function ShoppingList() {
-    return (
-        <ul>
-            {plantList.map((plant, index) => (
-                <li key={`${plant}-${index}`}>{ plant }</li>
-            ))}
-        </ul>
-    )
+	return <ul></ul>
 }
 
 export default ShoppingList
